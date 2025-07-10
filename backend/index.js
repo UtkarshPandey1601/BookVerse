f30802b9b54e36906bba44a9f7ec8f658cf85961
Utkarsh Pandey <up81451@gmail.com>
2025-07-10 15:18:18 +0530

    Project Completed

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+import bookRoutes from "./routes/bookroutes.js";
+import router from "./routes/userroutes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURL;
+
+try {
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("Connected to mongoDB");
+} catch (error) {
+    console.log("Error: ", error);
+}
+
+app.use("/book", bookRoutes);
+app.use("/user", router);
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static("frontend/dist"));
+    
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "./frontend/dist", "index.html"));
+    });
+}
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
